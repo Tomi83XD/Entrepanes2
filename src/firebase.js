13f14155385a2c38185377e744604bc46d3f0c82
Tomi83XD <tomaspro83xd@gmail.com>
2025-11-05 10:12:56 -0300

    env

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,23 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; 
-import { getFirestore } from "firebase/firestore"; 
+  // src/firebase.js
+  import { initializeApp } from "firebase/app";
+  import { getAuth } from "firebase/auth"; 
+  import { getFirestore } from "firebase/firestore"; 
 
-// --- Configuración de Firebase ---
-const firebaseConfig = {
-    apiKey: "AIzaSyAvBWbWkhEooaUvc2cPByOx-Q-JEjhRNnQ",
-    authDomain: "entrepanesvcp-ad077.firebaseapp.com",
-    projectId: "entrepanesvcp-ad077",
-    storageBucket: "entrepanesvcp-ad077.firebasestorage.app",
-    messagingSenderId: "230894274953",
-    appId: "1:230894274953:web:2d577123b8c1988d1db958",
-    measurementId: "G-LTJL6CQVMF"
-};
+  // --- Configuración de Firebase ---
+  const firebaseConfig = {
+      apiKey: "AIzaSyAvBWbWkhEooaUvc2cPByOx-Q-JEjhRNnQ",
+      authDomain: "entrepanesvcp-ad077.firebaseapp.com",
+      databaseURL: "https://entrepanesvcp-ad077-default-rtdb.firebaseio.com",
+      projectId: "entrepanesvcp-ad077",
+      storageBucket: "entrepanesvcp-ad077.firebasestorage.app",
+      messagingSenderId: "230894274953",
+      appId: "1:230894274953:web:2d577123b8c1988d1db958",
+      measurementId: "G-LTJL6CQVMF"
+  };
 
-const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
 
-// Exportación de servicios (CRÍTICO: Aquí se definen 'auth' y 'db' que el Admin.jsx necesita)
-export const auth = getAuth(app); 
-export const db = getFirestore(app); 
+  // Exportación de servicios (CRÍTICO: Aquí se definen 'auth' y 'db' que el Admin.jsx necesita)
+  export const auth = getAuth(app); 
+  export const db = getFirestore(app); 
+  
